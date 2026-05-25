ef97240dd33ab5653b649a2c4657b660909aa108
Amein <amein@gmail.com>
2026-05-25 10:45:28 +0200

    Kombiner WiFi sniffing med JSON output

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,36 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include "esp_wifi.h"
 
-// Placeholder WiFi-info
-const char* ssid = "DIT_WIFI";
-const char* password = "DIT_PASSWORD";
-
-// Placeholder MQTT broker IP
-const char* mqtt_server = "192.168.1.100";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup_wifi()
+void macToString(const uint8_t *mac, char *macStr)
 {
-  delay(100);
-
-  Serial.println();
-  Serial.print("Forbinder til WiFi: ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println();
-  Serial.println("WiFi forbundet");
+  snprintf(macStr, 18, "%02X:%02X:%02X:%02X:%02X:%02X",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
-void reconnect()
+void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 {
-  while (!client.connected())
-  {
-    Serial.print("Forsøger MQTT forbindelse...");
+  wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t*)buff;
 
-    if (client.connect("ESP32Client"))
-    {
-      Serial.println("forbundet");
-    }
-    else
-    {
-      Serial.print("fejl, rc=");
-      Serial.print(client.state());
-      Serial.println(" prøver igen om 5 sekunder");
-      delay(5000);
-    }
-  }
+  int rssi = pkt->rx_ctrl.rssi;
+  unsigned long timestamp = millis();
+
+  uint8_t *payload = pkt->payload;
+  uint8_t *mac = payload + 10;
+
+  char macStr[18];
+  macToString(mac, macStr);
+
+  String json =
+    "{"
+    "\"id\":\"" + String(macStr) + "\","
+    "\"timestamp\":" + String(timestamp) + ","
+    "\"rssi\":" + String(rssi) + ","
+    "\"x\":0,"
+    "\"y\":0"
+    "}";
+
+  Serial.println(json);
 }
 
 void setup()
@@ -57,38 +38,15 @@ void setup()
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("MQTT placeholder prototype startet");
+  Serial.println("WiFi sniffer JSON prototype startet");
 
-  // WiFi er slået fra indtil vi tester på skolen
-  // setup_wifi();
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
-  // MQTT server er defineret, men bruges ikke aktivt endnu
-  client.setServer(mqtt_server, 1883);
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
 }
 
 void loop()
 {
-  // MQTT forbindelse er slået fra indtil vi tester på skolen
-  // if (!client.connected())
-  // {
-  //   reconnect();
-  // }
-
-  // client.loop();
-
-  String payload =
-    "{"
-    "\"id\":\"placeholder-device-id\","
-    "\"timestamp\":123456,"
-    "\"x\":2.1,"
-    "\"y\":1.4,"
-    "\"rssi\":-67"
-    "}";
-
-  Serial.println(payload);
-
-  // MQTT publish er slået fra indtil vi tester på skolen
-  // client.publish("iot/position", payload.c_str());
-
-  delay(5000);
 }
